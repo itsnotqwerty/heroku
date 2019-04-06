b524fac3a716aa311d22265747757acc3cfe66bb
Sam Roux <samroux808@gmail.com>
2019-04-06 00:30:31 -0500

    Log mentions

--- main.ts
+++ main.ts
@@ -10,6 +10,9 @@ web.listen(PORT);
 bot.on('message', (message: discord.Message) => {
     console.log(message.content);
     console.log(message.mentions.members);
+    if ('109094301962625024' in messae.mentions.members) {
+        message.channel.send('Fuck off!');
+    }
 });
 
 bot.login('NTM1NTIzNzM3MTU2NTgzNDQ0.XKgv4Q.VX4OvA-lsds1RYkvZBe-uidaTM4');
