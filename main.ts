970507597873c574c4f2acaea688c434b1854eed
Sam Roux <samroux808@gmail.com>
2019-04-06 00:13:01 -0500

    Changes made

--- main.ts
+++ main.ts
@@ -1,5 +1,14 @@
 import * as discord from 'discord.js';
+import express from 'express';
 
 const bot: discord.Client = new discord.Client();
+const web: express.Express = express();
+const PORT = process.env.PORT || 5000
+
+web.listen(PORT);
+
+bot.on('message', (message: discord.Message) => {
+    console.log(message.content);
+})
 
 bot.login('NTM1NTIzNzM3MTU2NTgzNDQ0.XKgv4Q.VX4OvA-lsds1RYkvZBe-uidaTM4');
