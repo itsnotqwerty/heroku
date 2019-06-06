455c9849be04119fd5903b88557a2fef2c89cbd4
Sam Roux <samroux808@gmail.com>
2019-06-05 22:32:08 -0500

    Discord commands implemented

--- vars.ts
+++ vars.ts
@@ -23,46 +23,84 @@ export const commands = [
     {
         'trigger': '::points',
         'response': async (packet: CommandPacket) => {
-            return `${packet.user} has ${packet.user.points} points`;
+            switch (packet.platform) {
+                case "discord":
+                    return `you have ${packet.user.points} points`;
+                default:
+                    return `${packet.user.username} has ${packet.user.points} points`;
+            }
         }
     },
     {
         'trigger': '::setpoints',
         'response': async (packet: CommandPacket) => {
-            if (packet.user.admin != true) { return `@${packet.user}, you don't have permission to use that!` };
-            if (packet.args.length != 3) { return `@${packet.user}, you're not using the command properly!` };
+            switch (packet.platform) {
+                case "discord":
+                    if (packet.user.admin != true) { return `you don't have permission to use that!` };
+                    if (packet.args.length != 3) { return `you're not using the command properly!` };
+                default:
+                    if (packet.user.admin != true) { return `${packet.user.username}, you don't have permission to use that!` };
+                    if (packet.args.length != 3) { return `${packet.user.username}, you're not using the command properly!` };
+            }
             let target = mongoCon.findUser(packet.args[1]);
             if (target == null) {
                 await mongoCon.insertUser(packet.args[1]);
             }
             await mongoCon.setPoints(packet.args[1], parseInt(packet.args[2]));
-            return `${packet.args[1]} now has ${parseInt(packet.args[2])} points`;
+            switch (packet.platform) {
+                case "discord":
+                    return `<@${packet.args[1]}> now has ${packet.user.points + parseInt(packet.args[2])} points`;
+                default:
+                    return `${packet.args[1]} now has ${packet.user.points + parseInt(packet.args[2])} points`;
+            }
         }
     },
     {
         'trigger': '::addpoints',
         'response': async (packet: CommandPacket) => {
-            if (packet.user.admin != true) { return `@${packet.user}, you don't have permission to use that!` };
-            if (packet.args.length != 3) { return `@${packet.user}, you're not using the command properly!` };
+            switch (packet.platform) {
+                case "discord":
+                    if (packet.user.admin != true) { return `you don't have permission to use that!` };
+                    if (packet.args.length != 3) { return `you're not using the command properly!` };
+                default:
+                    if (packet.user.admin != true) { return `${packet.user.username}, you don't have permission to use that!` };
+                    if (packet.args.length != 3) { return `${packet.user.username}, you're not using the command properly!` };
+            }
             let target: User = await mongoCon.findUser(packet.args[1]);
             if (target == null) {
                 await mongoCon.insertUser(packet.args[1]);
             }
             await mongoCon.addPoints(packet.args[1], parseInt(packet.args[2]));
-            return `${packet.args[1]} now has ${parseInt(packet.args[2])} more points`;
+            switch (packet.platform) {
+                case "discord":
+                    return `<@${packet.args[1]}> now has ${parseInt(packet.args[2])} more points`;
+                default:
+                    return `${packet.args[1]} now has ${parseInt(packet.args[2])} more points`;
+            }
         }
     },
     {
         'trigger': '::subpoints',
         'response': async (packet: CommandPacket) => {
-            if (packet.user.admin != true) { return `@${packet.user}, you don't have permission to use that!` };
-            if (packet.args.length != 3) { return `@${packet.user}, you're not using the command properly!` };
+            switch (packet.platform) {
+                case "discord":
+                    if (packet.user.admin != true) { return `you don't have permission to use that!` };
+                    if (packet.args.length != 3) { return `you're not using the command properly!` };
+                default:
+                    if (packet.user.admin != true) { return `${packet.user.username}, you don't have permission to use that!` };
+                    if (packet.args.length != 3) { return `${packet.user.username}, you're not using the command properly!` };
+            }
             let target: User = await mongoCon.findUser(packet.args[1]);
             if (target == null) {
                 await mongoCon.insertUser(packet.args[1]);
             }
             await mongoCon.subPoints(packet.args[1], parseInt(packet.args[2]));
-            return `${packet.args[1]} now has ${parseInt(packet.args[2])} less points`;
+            switch (packet.platform) {
+                case "discord":
+                    return `<@${packet.args[1]}> now has ${parseInt(packet.args[2])} less points`;
+                default:
+                    return `${packet.args[1]} now has ${parseInt(packet.args[2])} less points`;
+            }
         }
     }
 ]
