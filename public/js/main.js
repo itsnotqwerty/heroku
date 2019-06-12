b994cca6dc378aa1f70e455e9e79a768c0fe5f53
Sam Roux <samroux808@gmail.com>
2019-06-11 22:34:32 -0500

    p5.js!

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,15 @@
+var W = windowWidth;
+var H = windowHeight;
+
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(W, H);
 }
 
 function draw() {
-	ellipse(0, 0, windowWidth, windowHeight);
+    background(255);
+	ellipse(W/2, H/2, W, H);
+}
+
+function windowResized() {
+    resizeCanvas(W, H);
 }
