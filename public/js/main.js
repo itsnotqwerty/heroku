c3c55adb03f32c384879a9ab0636143485c993a6
Sam Roux <samroux808@gmail.com>
2019-06-11 22:46:34 -0500

    p5.js!

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,21 @@
 var W = window.innerWidth;
 var H = window.innerHeight;
 
+var balls = [
+    {x: 50, y: 50},
+    {x: 100, y: 100},
+    {x: 250, y: 250}
+]
+
 function setup() {
     createCanvas(W, H);
 }
 
 function draw() {
     background(255);
-	ellipse(W/2, H/2, W, H);
+	for (let ball of balls) {
+        ellipse(ball.x, ball.y, 50, 50);
+    }
 }
 
 function windowResized() {
