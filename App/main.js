30f0fa8a88ec55fb4034e571709d023d757a3f79
Sam Roux <samroux808@gmail.com>
2019-06-26 00:09:38 -0500

    Index - Port fixes?

--- App/main.js
+++ App/main.js
@@ -1,13 +1,11 @@
-import express = require("express");
-import io = require("socket.io");
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
 const app = express();
-
 app.set('port', process.env.PORT || 3000);
 app.set('views', './Views');
 app.set('view engine', 'pug');
 app.use(express.static('public'));
-
 app.use((req, res) => {
     return res.render('index');
 });
