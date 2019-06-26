fdc8155c33e771e80d01693bc1f1997a95b0ca6c
Sam Roux <samroux808@gmail.com>
2019-06-26 00:13:55 -0500

    Index - Now it works! (TM)

--- App/main.ts
+++ App/main.ts
@@ -3,11 +3,12 @@ import io = require("socket.io");
 
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
-app.set('views', './Views');
+app.set('views', __dirname + '/Views');
 app.set('view engine', 'pug');
 app.use(express.static('public'));
 
 app.use((req, res) => {
     return res.render('index');
 });
+
+app.listen(process.env.PORT || 8080);
