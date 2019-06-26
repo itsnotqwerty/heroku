6d160e809da23b82e429f81f21d610972ee9d81a
Sam Roux <samroux808@gmail.com>
2019-06-26 13:59:32 -0500

    Styles - Added styles

--- App/main.ts
+++ App/main.ts
@@ -1,17 +1,16 @@
 import express = require("express");
 import io = require("socket.io");
-import { Http2SecureServer } from "http2";
 import { get } from "http";
 
 const app = express();
 
 app.set('views', __dirname + '/Views');
 app.set('view engine', 'pug');
-app.use(express.static('public'));
+app.use(express.static('Public'));
 
 app.use((req, res) => {
     return res.render('index', {
-        url: req.url
+        title: req.path
     });
 });
 
