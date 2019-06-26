175b683b25ff546ed5654f613dd193bed188be34
Sam Roux <samroux808@gmail.com>
2019-06-26 14:02:24 -0500

    Index - Public directories fixed?

--- App/main.ts
+++ App/main.ts
@@ -6,7 +6,7 @@ const app = express();
 
 app.set('views', __dirname + '/Views');
 app.set('view engine', 'pug');
-app.use(express.static('Public'));
+app.use('/', express.static('Public'));
 
 app.use((req, res) => {
     return res.render('index', {
