1bdd4ddb0184236ad1147491a21f8c2bbb2804ce
Sam Roux <samroux808@gmail.com>
2019-06-27 00:11:23 -0500

    Index - HTTPS

--- App/main.js
+++ App/main.js
@@ -1,24 +1,19 @@
-import express = require("express");
-import io = require("socket.io");
-import { get } from "http";
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const http_1 = require("http");
 const app = express();
-
 app.set('views', __dirname + '/Views');
 app.set('view engine', 'pug');
-
 app.use('/styles', express.static(__dirname + '/Public/Styles'));
 app.use('/images', express.static(__dirname + '/Public/Images'));
 app.use('/videos', express.static(__dirname + '/Public/Videos'));
-
 app.use((req, res) => {
     return res.render('index', {
         title: 'DataBin'
     });
 });
-
 app.listen(process.env.PORT || 8080);
-
 setInterval(() => {
-    get('https://projectseveryweek.com/')
+    http_1.get('http://infinite-dusk-64948.herokuapp.com/');
 }, 300000);
