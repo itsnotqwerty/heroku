faf4fedb87c8adf6140f181e37304609475fed58
Sam Roux <samroux808@gmail.com>
2019-06-29 22:03:45 -0500

    Sockets - Switched back to Socket.io

--- App/Entities/mongo.ts
+++ App/Entities/mongo.ts
@@ -3,23 +3,21 @@ import { Document, PrivateUser, Review, LoginPacket } from "./entities";
 
 
 export class MongoCon {
-    private mongoCli: MongoClient;
     private db: string;
 
     constructor() {
-        this.mongoCli = new MongoClient(process.env.MONGO_URI!, {useNewUrlParser: true});
         this.db = process.env.MONGO_DBNAME!;
     }
 
     async insertReview(entity: Document<Review>) {
-        this.mongoCli.connect().then((cli: MongoClient) => {
+        MongoClient.connect(process.env.MONGO_URI!, {useNewUrlParser: true}).then((cli: MongoClient) => {
             cli.db(this.db).collection('reviews').insertOne(entity);
             cli.close();
         });
     }
 
     async insertUser(entity: Document<PrivateUser>) {
-        this.mongoCli.connect().then((cli: MongoClient) => {
+        MongoClient.connect(process.env.MONGO_URI!, {useNewUrlParser: true}).then((cli: MongoClient) => {
             cli.db(this.db).collection('users').insertOne(entity);
             cli.close();
         });
