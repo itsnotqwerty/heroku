dea51335f37db565d4043e4f91adee95328e51eb
Sam Roux <samroux808@gmail.com>
2019-06-30 15:58:46 -0500

    Starting from scratch

--- App/Modules/loader.ts
+++ App/Modules/loader.ts
@@ -2,9 +2,9 @@
     DO NOT EDIT LINES FIVE THROUGH TEN! IT IS MEANT TO SHOW NEW USERS THE ROPES!
 */
 
-import express = require("express");
-const app = express();
-
 import { ExampleModule } from "./Example/example";
 const example = new ExampleModule();
+import express = require("express");
+const app = express();
 example.load(app);
+app.listen(process.env.PORT || 8080)
