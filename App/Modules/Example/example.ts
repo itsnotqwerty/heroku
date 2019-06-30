b1a17ad98c9fee1fe498a086c7128458afec9555
Sam Roux <samroux808@gmail.com>
2019-06-30 15:59:57 -0500

    Starting from scratch

--- App/Modules/Example/example.ts
+++ App/Modules/Example/example.ts
@@ -6,7 +6,7 @@ import * as core from "express-serve-static-core";
 
 export class ExampleModule {
     load(app: core.Express) {
-        app.set('views', './Views');
+        app.set('views', '/Example/Views');
         app.set('view engine', 'pug');
         app.use((req, res) => {
             return res.render('index');
